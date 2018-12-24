a71a05a8e961b2fb1432a6ed0b9ad5e251d5b4fa
Zhao Yao <yao.zhao@pipup.me>
2018-12-24 18:22:06 +0800

    读写文件

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
-	"net/http"
 	"io/ioutil"
+	"fmt"
+	"strconv"
+	"os"
+	"net/http"
 	"encoding/json"
 	"strings"
-	"strconv"
 )
 
 
 type AirQuality struct{
 	Aqi       int  `json:"aqi"`
+}
+
+func IsOverproof(filename string) bool {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b, err := strconv.ParseBool(string(content))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return b
+}
+
+func writeFile(filename string)  {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.WriteString("true")
 }
 
 func main() {
@@ -24,7 +47,12 @@ func main() {
 	var air AirQuality
 	json.Unmarshal(body, &air)
 
-	http.Post("https://api.telegram.org/bot705617182:AAHyw5JrrlWCQf-D2l5X1fLtXJE8plJqtOU/sendMessage",
-		"application/x-www-form-urlencoded",
-		strings.NewReader("chat_id=-321414996&text=pm2.5超标了！已经达到 "+strconv.Itoa(air.Aqi)))
+	isOverproof := IsOverproof("data.txt")
+
+	if isOverproof {
+		writeFile("data.txt")
+		http.Post("https://api.telegram.org/bot705617182:AAHyw5JrrlWCQf-D2l5X1fLtXJE8plJqtOU/sendMessage",
+			"application/x-www-form-urlencoded",
+			strings.NewReader("chat_id=-321414996&text=pm2.5超标了！已经达到 "+strconv.Itoa(air.Aqi)))
+	}
 }
