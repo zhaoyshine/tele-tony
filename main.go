6e5d0076a12c75438ce9889eadec226bb522e26d
Zhao Yao <yao.zhao@pipup.me>
2018-12-27 09:45:32 +0800

    测试

--- main.go
+++ main.go
@@ -87,7 +87,7 @@ func main() {
 		resBody, _ := ioutil.ReadAll(resp.Body)
 		fmt.Println(string(resBody))
 	}
-	if !isToday {
+	if isToday {
 		resp, err := http.Post("https://api.telegram.org/bot705617182:AAHyw5JrrlWCQf-D2l5X1fLtXJE8plJqtOU/sendMessage",
 			"application/x-www-form-urlencoded",
 			strings.NewReader("chat_id=-321414996&text=新的一天！现在的pm2.5是 "+strconv.Itoa(air.Aqi)+"。 \n"+string(sayingBody)))
